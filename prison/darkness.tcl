ee6a31ae98f2279c805ddbe15a41ef6a4667fead
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-11-06 22:24:38 -0500

    The final battle of false stage

--- prison/darkness.tcl
+++ prison/darkness.tcl
@@ -75,9 +75,90 @@ namespace eval Prison::Darkness {
     }
 
     proc arcadeDown {} {
-        # //// The "final battle" here (consisting of all the engaged NPCs),
-        # then resetting the conditions back to the beginning.
-        return -gameover
+        puts "== Arcade Basement =="
+        puts -nonewline "Silver Starlight is already at the shrine, standing opposite\
+        a shadow which has taken the form of one of the dancers on the stage."
+        set engaged [list]
+        set linking "is"
+        if {[state get spirit-bald]} then {
+            lappend engaged "the bald man from the auditorium"
+        }
+        if {[state get spirit-lizard]} then {
+            lappend engaged "the lizard man from the theater"
+        }
+        if {[state get spirit-gamer]} then {
+            lappend engaged "the gamer"
+        }
+        if {[state get spirit-baker]} then {
+            lappend engaged "the baker"
+        }
+        if {[llength $engaged] > 0} then {
+            set engaged [lreplace $engaged 0 0 [string toupper [lindex $engaged 0] 0 0]]
+        }
+        if {[llength $engaged] > 1} then {
+            set index [expr {[llength $engaged] - 1}]
+            set engaged [lreplace $engaged $index $index "and [lindex $engaged $index]"]
+            set linking "are"
+        }
+        if {[llength $engaged] > 2} then {
+            set text [join $engaged ", "]
+        } else {
+            set text [join $engaged " "]
+        }
+        if {[llength $engaged] > 0} then {
+            puts " $text $linking standing behind the dancer, also now veiled behind shadows.\
+            Silver Starlight speaks first."
+        } else {
+            puts " Several smaller shadows encircle the dancer. Silver Starlight speaks first."
+        }
+        puts "\"Took you long enough. You're just in time to see me finish the job!\""
+        state put spirit-bald no
+        state put spirit-lizard no
+        state put spirit-gamer no
+        state put spirit-baker no
+        state put false-stage no
+        if {[llength $engaged] >= 4} then {
+            # //// There will be a second case here if you have some specific item
+            puts "The five shadow beings move together and begin to morph into one\
+            large shadow monster, much taller than either you or Starlight. Silver\
+            Starlight doesn't miss a beat, immediately beginning to chant a spell,\
+            but her magic has little effect on the monster. The monster speaks with\
+            a deep, distorted voice."
+            puts "\"You stand no chance against us, mortals.\""
+            puts "In one attack, the monster attacks both of you, and a white light engulfs\
+            you. When the light clears, you find yourselves back in the small shed, the\
+            portal on the back wall still open."
+            return arcadeDownLoss
+        } else {
+            puts "Silver Starlight holds her scepter high and dispels all of the shadows\
+            in one attack. As soon as she does so, a white light engulfs both of you. When\
+            the light clears, you find yourself back in the shed with Starlight. The portal\
+            is still open on the back wall."
+            return arcadeDownEasy
+        }
+    }
+
+    proc arcadeDownEasy {} {
+        # //// I need to update her dialogue the second time through to acknowledge that we've
+        # already done it all, even though everything will mechanically be the same
+        puts "\"That... didn't feel like it worked. I don't think all of the spirits were\
+        there. I think we need to make sure all of the spirits are down in that shrine\
+        before we fight them. If you're ready to go back through and try again, just let\
+        me know.\""
+        prompt {} {
+            {"\"Okay.\"" yes ::Prison::Cottage::shed}
+        }
+    }
+
+    proc arcadeDownLoss {} {
+        # //// Once we've decided which item is going to result in a victory, Starlight will
+        # give a hint about it, at least telling us what to look for
+        puts "\"That... didn't work out. I guess we just need a bit more power, but at least\
+        we managed to gather all of the spirits in one place. Let me know if you want to go\
+        back and try again.\""
+        prompt {} {
+            {"\"Okay.\"" yes ::Prison::Cottage::shed}
+        }
     }
 
     proc bakery {} {
