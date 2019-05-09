c40c6bde0c51c38f18fc0dc91afe20afba95d9df
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-05-08 23:51:06 -0400

    Note for what to do next

--- city/science.tcl
+++ city/science.tcl
@@ -106,7 +106,7 @@ namespace eval City::Science {
             return mainRoom
         }
         puts "== Diamond Room =="
-        # //// Empty room right now
+        # ///// Empty room right now
         prompt {} {
             {"Go back out" yes mainRoom}
         }
