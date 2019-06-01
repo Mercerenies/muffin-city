8333d510196f4bff88acd6a7dab8b4360c4ae49b
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-06-01 16:23:53 -0400

    Debug of Mister Midas and the Mastermind

--- prison/north.tcl
+++ prison/north.tcl
@@ -45,12 +45,16 @@ namespace eval Prison::North {
             ([state get brain-control] eq {no})} then {
             state put brain-control initiated
         }
+        if {[state get brain-control] in {washroom washroom1}} then {
+            puts -nonewline " A bald man with a goatee is staring at the palm of his hand."
+        }
         puts {}
         prompt {} {
             {"Talk to the gentleman" {[state get harry-location] eq {no}} harry}
             {"Talk to Harry" {[state get harry-location] in {met breakout}} harry}
+            {"Talk to the bald man" {[state get brain-control] in {washroom washroom1}} restBald}
             {"Take the coin" {[state get washroom-coin] eq {ready}} restCoin}
-            {"Exit the washroom" yes hallway}
+            {"Exit the washroom" yes toHallway}
         }
     }
 
@@ -129,13 +133,88 @@ namespace eval Prison::North {
         return restroom
     }
 
+    proc restBald {} {
+        puts "\"Jewels... gems... so shiny... I want them all...\""
+        state put brain-control washroom1
+        puts {}
+        return restroom
+    }
+
+    proc toHallway {} {
+        if {[state get brain-control] eq {washroom1}} then {
+            return hypnoConfront
+        } else {
+            return hallway
+        }
+    }
+
+    proc hypnoConfront {} {
+        puts "As you turn to leave, a stout man in a top hat, black mask, and full cloak\
+        enters the washroom and blocks your exit."
+        puts "\"Ah, it seems I've been caught in the act. Greetings, puppet. You may call\
+        me the Mastermind.\""
+        prompt {} {
+            {"\"And what exactly are you doing here?\"" yes hypnoConfrontAsk}
+            {"\"I'm not a puppet.\"" yes hypnoConfrontNoPuppet}
+            {"\"Goodbye now.\"" yes hypnoConfrontGoodbye}
+        }
+    }
+
+    proc hypnoConfrontAsk {} {
+        puts "\"Quite simply, I'm practicing my hypnosis abilities. Unfortunately for you,\
+        you've already seen too much.\""
+        return hypnoTry
+    }
+
+    proc hypnoConfrontNoPuppet {} {
+        puts "\"Well, not yet. But this provides an excellent opportunity for me\
+        to practice my hypnosis abilities.\""
+        return hypnoTry
+    }
+
+    proc hypnoConfrontGoodbye {} {
+        puts "\"I'm afraid I can't let you leave. You've simply seen too much, puppet.\
+        I'll have to practice my hypnosis abilities on you.\""
+        return hypnoTry
+    }
+
+    proc hypnoTry {} {
+        puts "The Mastermind reaches into his pocket and withdraws a pendulum, which\
+        he swings in front of your face. The bald man immediately starts staring at\
+        the pendulum."
+        prompt {} {
+            {"\"Is... this supposed to be doing anything?\"" yes hypnoNoEffect}
+            {"\"Can I leave now?\"" yes hypnoNoEffect}
+        }
+    }
+
+    proc hypnoNoEffect {} {
+        puts "\"Hm... that usually works. Ah, well, plan B then. Midas, take good\
+        care of our new friend.\""
+        puts "The bald man pulls a small knife out of his belt and thrusts it into\
+        your chest."
+        state put brain-control hiding
+        if {[state get lobby-door] ne {yes}} then {
+            state put lobby-door murder
+        }
+        puts {}
+        return ::Underworld::Lobby::murder
+    }
+
     proc dining {} {
         puts "== Dining Hall =="
-        puts "The dining hall consists of a small collection of tables opposite a\
-        cafeteria-like buffet. The stand is unfortunately empty, as it is not meal\
-        time, but there are a few prisoners socializing in the corner."
+        puts -nonewline "The dining hall consists of a small collection of tables\
+        opposite a cafeteria-like buffet. The stand is unfortunately empty, as it\
+        is not meal time, but there are a few prisoners socializing in the corner."
+        if {[state get brain-control] eq {no}} then {
+            puts " On the other side of the room, a bald man with a goatee is staring\
+            at the palm of his hand."
+        } else {
+            puts {}
+        }
         prompt {} {
             {"Talk to the prisoners" yes diningTalk}
+            {"Talk to the bald man" {[state get brain-control] eq {no}} diningBald}
             {"Go back to the hall" yes hallway}
         }
     }
@@ -157,6 +236,12 @@ namespace eval Prison::North {
         yer [state get stolen-good] now. But hey, no hard feelings for turnin' me in. I'll get\
         outta here soon enough.\""
         state put city-thug caught
+        puts {}
+        return dining
+    }
+
+    proc diningBald {} {
+        puts "\"Jewels... gems... so shiny... I want them all...\""
         puts {}
         return dining
     }
