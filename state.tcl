dec35371dd0d56b611c875d68486d8ab992f5729
Silvio Mayolo <mercerenies@comcast.net>
2020-06-18 18:27:03 -0400

    Fix for compatibility with older Tcl versions

--- state.tcl
+++ state.tcl
@@ -1,147 +1,147 @@
 
 namespace eval State {
 
-    variable impl [dict create {*}{
-        city-thug no
-        stolen-good {}
-        thug-card {Platinum Card}
-        sa-coin no
-        trial-crime no
-        trial-reason {}
-        janitor-door no
-        been-to-prison no
-        prison-guard no
-        awaiting-bus no
-        forest-river 0
-        lobby-door no
-        talked-to-johnny no
-        johnny-quest no
-        exercise-soul no
-        talked-to-cipher no
-        second-class-door no
-        first-class-door no
-        muffin-second no
-        motel-room no
-        inn-room no
-        heard-science no
-        butler-game no
-        talked-to-louis no
-        jumped-into-fire no
-        collected-suit no
-        fe-coin no
-        merchant-bot no
-        olive-bought no
-        secret-chamber-door no
-        attorney-man no
-        courtroom-key no
-        subspace-attic no
-        taco-shop no
-        pawn-shop-pass no
-        has-suitcase no
-        hero-blade no
-        hero-crystal no
-        subspace-portal no
-        attorney-self no
-        attorney-thug no
-        attorney-guard no
-        courthouse-door no
-        judge-muffin no
-        guard-soul no
-        hunter-trail no
-        hunter-soul no
-        mercury-muffin no
-        moon-teleport no
-        moon-mechanic no
-        moon-train no
-        subspace-reason no
-        talked-to-acolyte no
-        fire-pit no
-        talked-to-steve no
-        necro-cipher no
-        golden-arches no
-        talked-to-reaper no
-        reaper-has-item no
-        reaper-helper no
-        stairs-tried no
-        talked-to-grigory no
-        captain-boat no
-        motel-ghost no
-        motel-prison no
-        motel-warehouse no
-        motel-subspace no
-        captain-boat-place dream
-        talked-to-todd no
-        merchant-war no
-        judge-paperclip no
-        the-fence no
-        cottage-spirit no
-        false-stage no
-        spirit-baker no
-        spirit-gamer no
-        spirit-bald no
-        spirit-muffin no
-        spirit-lizard no
-        false-stage-ran no
-        stage-coin no
-        subspace-freedom no
-        reaper-blessing no
-        obtained-true-reaper no
-        steve-disappeared no
-        pirate-attack no
-        talked-to-hatman no
-        treasure-room no
-        harry-location no
-        police-hut no
-        baby-doll no
-        hut-password no
-        heart-pipe no
-        moth-king no
-        doll-key no
-        rocket-launched no
-        washroom-coin no
-        brain-control no
-        crypto-king no
-        crypto-door no
-        merchant-fought no
-        merchant-atheena no
-        merchant-starlight no
-        warehouse-owner no
-        owner-key no
-        resolved-todd no
-        lantern-bought no
-        lantern-muffin no
-        train-revisit no
-        dining-hall no
-        spicy-visit 0
-        spicy-return no
-        met-robot no
-        met-mesmerist no
-        robot-hypnotism no
-        been-to-school no
-        school-period no
-        first-period-pass no
-        second-period-pass no
-        third-period-pass no
-        authorized-to-bus no
-        school-flying-colors no
-        topaz-rescue no
-        cave-hideout no
-        escape-debriefed no
-        king-war no
-        drawbridge-down no
-        moon-research no
-        know-about-moon-war no
+    variable impl [dict create \
+        city-thug no \
+        stolen-good {} \
+        thug-card {Platinum Card} \
+        sa-coin no \
+        trial-crime no \
+        trial-reason {} \
+        janitor-door no \
+        been-to-prison no \
+        prison-guard no \
+        awaiting-bus no \
+        forest-river 0 \
+        lobby-door no \
+        talked-to-johnny no \
+        johnny-quest no \
+        exercise-soul no \
+        talked-to-cipher no \
+        second-class-door no \
+        first-class-door no \
+        muffin-second no \
+        motel-room no \
+        inn-room no \
+        heard-science no \
+        butler-game no \
+        talked-to-louis no \
+        jumped-into-fire no \
+        collected-suit no \
+        fe-coin no \
+        merchant-bot no \
+        olive-bought no \
+        secret-chamber-door no \
+        attorney-man no \
+        courtroom-key no \
+        subspace-attic no \
+        taco-shop no \
+        pawn-shop-pass no \
+        has-suitcase no \
+        hero-blade no \
+        hero-crystal no \
+        subspace-portal no \
+        attorney-self no \
+        attorney-thug no \
+        attorney-guard no \
+        courthouse-door no \
+        judge-muffin no \
+        guard-soul no \
+        hunter-trail no \
+        hunter-soul no \
+        mercury-muffin no \
+        moon-teleport no \
+        moon-mechanic no \
+        moon-train no \
+        subspace-reason no \
+        talked-to-acolyte no \
+        fire-pit no \
+        talked-to-steve no \
+        necro-cipher no \
+        golden-arches no \
+        talked-to-reaper no \
+        reaper-has-item no \
+        reaper-helper no \
+        stairs-tried no \
+        talked-to-grigory no \
+        captain-boat no \
+        motel-ghost no \
+        motel-prison no \
+        motel-warehouse no \
+        motel-subspace no \
+        captain-boat-place dream \
+        talked-to-todd no \
+        merchant-war no \
+        judge-paperclip no \
+        the-fence no \
+        cottage-spirit no \
+        false-stage no \
+        spirit-baker no \
+        spirit-gamer no \
+        spirit-bald no \
+        spirit-muffin no \
+        spirit-lizard no \
+        false-stage-ran no \
+        stage-coin no \
+        subspace-freedom no \
+        reaper-blessing no \
+        obtained-true-reaper no \
+        steve-disappeared no \
+        pirate-attack no \
+        talked-to-hatman no \
+        treasure-room no \
+        harry-location no \
+        police-hut no \
+        baby-doll no \
+        hut-password no \
+        heart-pipe no \
+        moth-king no \
+        doll-key no \
+        rocket-launched no \
+        washroom-coin no \
+        brain-control no \
+        crypto-king no \
+        crypto-door no \
+        merchant-fought no \
+        merchant-atheena no \
+        merchant-starlight no \
+        warehouse-owner no \
+        owner-key no \
+        resolved-todd no \
+        lantern-bought no \
+        lantern-muffin no \
+        train-revisit no \
+        dining-hall no \
+        spicy-visit 0 \
+        spicy-return no \
+        met-robot no \
+        met-mesmerist no \
+        robot-hypnotism no \
+        been-to-school no \
+        school-period no \
+        first-period-pass no \
+        second-period-pass no \
+        third-period-pass no \
+        authorized-to-bus no \
+        school-flying-colors no \
+        topaz-rescue no \
+        cave-hideout no \
+        escape-debriefed no \
+        king-war no \
+        drawbridge-down no \
+        moon-research no \
+        know-about-moon-war no \
         abduction-escape no
-    }]
+    ]
 
-    proc get {args} {
+    proc get {arg} {
         variable impl
-        dict get $impl {*}$args
+        dict get $impl $arg
     }
 
-    proc put {args} {
+    proc put {arg v} {
         variable impl
-        dict set impl {*}$args
+        dict set impl $arg $v
     }
 
     proc all {} {
